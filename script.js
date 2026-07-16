3bb6016f5234db10d8e8652ca661061dc52825c9
sec24ad028-art <sec24ad028@sairamtap.edu.in>
2026-07-16 21:28:45 +0530

    Implement login functionality in script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function login(){
 
-alert("Welcome to AI-Powered Event Planning Assistant!");
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if(username==="admin" && password==="1234"){
+        alert("Login Successful!");
+        window.location.href="dashboard.html";
+    }else{
+        document.getElementById("message").innerHTML="Invalid Username or Password";
+    }
 
 }
