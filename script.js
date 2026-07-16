a2eca97c8aab8b057006d62aae47eb915c054208
sec24ad028-art <sec24ad028@sairamtap.edu.in>
2026-07-16 21:37:28 +0530

    Refactor login function to remove email/password check
    Simplified login function to redirect to dashboard without validation.

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-function login() {
-
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-
-    if(email !== "" && password !== ""){
-        window.location.href = "dashboard.html";
-    }
-    else{
-        alert("Please enter Email and Password");
-    }
-
+function login(){
+    window.location.href="dashboard.html";
 }
